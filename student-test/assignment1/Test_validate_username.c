e0354d08c466e0ee79a4bd92cdbe8af8e53526d4
jdemo <jdemo@Ubuntu.myguest.virtualbox.org>
2025-08-16 22:41:12 -0700

    autotest-validate updates

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -17,6 +17,15 @@ void test_validate_my_username()
     /**
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+     **/   
+   	
+ 	// get username
+	const char *expected = my_username();
+
+	// read username.txt
+	char *actual = malloc_username_from_conf_file();
+	
+	TEST_ASSERT_EQUAL_STRING_MESSAGE(expected, actual, "Username does not match");
+
+	// TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
 }
